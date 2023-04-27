a4c0361bea41607d34cb743f11b3832945d6d3a3
mohit <mohit.solanki@sculptsoft.com>
2023-04-27 15:52:17 +0530

    my first commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIAR2M5WYU3R5LICUXP"
+}
+variable "aws_secret_key" {
+default = "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"
+ }
+variable "region" {
+    default = "ap-south-1"
+}
