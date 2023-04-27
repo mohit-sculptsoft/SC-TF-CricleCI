0a2a8caa7c2eb56eb03296083f9a81bb9d403315
mohit <mohit.solanki@sculptsoft.com>
2023-04-27 16:27:18 +0530

    file updated for tfstorage

---
+++ config.tf
@@ -0,0 +1,8 @@
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+     bucket = "mohit-dev-terraform-storage"
+     key = "mohit-dev-terraform-storage/state.tfstate"
+     region = "ap-south-1"
+    }
+ }
