43badae5798794b48374c5d105650dc55052c17e
mohit <mohit.solanki@sculptsoft.com>
2023-05-03 16:39:15 +0530

    AWS cred passed

--- config.tf
+++ config.tf
@@ -1,10 +1,10 @@
 terraform {
   
-#   cloud {}
+  cloud {}
     required_version = ">= 0.12"
-    backend "s3" {
-     bucket = "mohit-dev-terraform-storage"
-     key = "mohit-dev-terraform-storage/state.tfstate"
-     region = "ap-south-1"
-    }
+    # backend "s3" {
+    #  bucket = "mohit-dev-terraform-storage"
+    #  key = "mohit-dev-terraform-storage/state.tfstate"
+    #  region = "ap-south-1"
+    # }
  }
