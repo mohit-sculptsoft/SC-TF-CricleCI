755abfee8a9a1a4fc00e49616e613e19ea33a241
mohit <mohit.solanki@sculptsoft.com>
2023-05-03 17:02:49 +0530

    Final testing

--- config.tf
+++ config.tf
@@ -2,6 +2,7 @@ terraform {
   
   cloud {}
     required_version = ">= 0.12"
+    
     # backend "s3" {
     #  bucket = "mohit-dev-terraform-storage"
     #  key = "mohit-dev-terraform-storage/state.tfstate"
