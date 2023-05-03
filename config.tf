38fa3fe15477aafeeed77c9c8a6f393d21b76411
mohit <mohit.solanki@sculptsoft.com>
2023-05-03 13:41:35 +0530

    tf cloud config changes applied

--- config.tf
+++ config.tf
@@ -1,8 +1,9 @@
 terraform {
+    cloud {}
     required_version = ">= 0.12"
-    backend "s3" {
-     bucket = "mohit-dev-terraform-storage"
-     key = "mohit-dev-terraform-storage/state.tfstate"
-     region = "ap-south-1"
-    }
+    # backend "s3" {
+    #  bucket = "mohit-dev-terraform-storage"
+    #  key = "mohit-dev-terraform-storage/state.tfstate"
+    #  region = "ap-south-1"
+    # }
  }
